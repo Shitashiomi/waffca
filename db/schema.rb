e53754a47a7875b3a25660fb0ff32ea2d3329a55
vagrant <vagrant@localhost.localdomain>
2020-12-06 12:55:00 +0000

    four commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_105712) do
+ActiveRecord::Schema.define(version: 2020_12_06_125142) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,6 +60,9 @@ ActiveRecord::Schema.define(version: 2020_12_03_105712) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
